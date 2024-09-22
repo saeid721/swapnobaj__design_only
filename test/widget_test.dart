2cdf4bd7ddd149c947ce7e9c96cc0820adb90b51
saeid721 <saeid721km@gmail.com>
2024-09-22 12:46:01 +0600

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:swapnabaj/main.dart';
 
-import 'package:swapnobaj_design_only/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SwapnobajApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
